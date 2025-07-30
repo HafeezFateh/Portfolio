571f2d7c150c71ac1fdb5eed6c51783dac0b694f
Hafeezur Rahman <hafeezofficial08@gmail.com>
2025-07-30 15:25:48 +0530

    V.1.1.4

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -11,32 +11,32 @@ function Portfolio(){
                     <div className="mx-5 mt-3 grid gap-5 grid-cols-1 lg:grid-cols-3">
                        
                        <ProjectCard
-                        image="./images/eruditex.png"
+                        image="/eruditex.png"
                         title="Erudite-X website"
                         link="https://erudite-x-static-website.vercel.app/"
                        />
                        <ProjectCard
-                        image="./images/townora.png"
+                        image="/townora.png"
                         title="Townora - Web app"
                         link="https://visharam-app.vercel.app/"
                        />
                        <ProjectCard
-                        image="./images/cahcet.png"
+                        image="/cahcet.png"
                         title="CAHCET - Official college website"
                         link="https://cahcet.in/"
                        />
                        <ProjectCard
-                        image="./images/stufotech.png"
+                        image="/stufotech.png"
                         title="StuFoTech - College web app"
                         link="#"
                        />
                        <ProjectCard
-                        image="./images/xameedar.png"
+                        image="/xameedar.png"
                         title="Xameedar - Land buy & sell website"
                         link="https://xameedar-i1kz.vercel.app/"
                        />
                        <ProjectCard
-                        image="./images/falah.png"
+                        image="/falah.png"
                         title="Al Falah - NGO website"
                         link="https://al-falah-foundation-f6fv.vercel.app/"
                        />
